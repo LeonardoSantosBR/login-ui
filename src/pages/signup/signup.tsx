bd88dde82a9f11ff06e7808b8987d0aa49e72888
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-01-31 23:16:43 -0300

    fix: comentarios retirados

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -23,17 +23,14 @@ function SignUp() {
   return (
     <div className="w-full min-h-screen flex justify-center items-center px-4">
       <div className="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl min-h-[50%] rounded-md bg-login_background_5 shadow-lg p-6">
-        {/* Ícone no topo */}
         <div className="w-full flex justify-center items-center py-4">
           <BadgePlus size={60} color="white" />
         </div>
 
-        {/* Formulário */}
         <form
           onSubmit={handleSubmit(handleSignup)}
           className="flex flex-col gap-4"
         >
-          {/* Campo Nome */}
           <div className="flex flex-col gap-2">
             <Controller
               control={control}
@@ -54,7 +51,6 @@ function SignUp() {
             <ErrorMessage message={formState.errors.name?.message} />
           </div>
 
-          {/* Campo Email */}
           <div className="flex flex-col gap-2">
             <Controller
               control={control}
@@ -79,7 +75,6 @@ function SignUp() {
             <ErrorMessage message={formState.errors.email?.message} />
           </div>
 
-          {/* Campo Senha */}
           <div className="flex flex-col gap-2">
             <Controller
               control={control}
@@ -100,7 +95,6 @@ function SignUp() {
             <ErrorMessage message={formState.errors.password?.message} />
           </div>
 
-          {/* Botão de Cadastro */}
           <div className="w-full flex flex-col items-center gap-3">
             <CreateClientButton />
             <p className="text-zinc-50 underline hover:text-gray-300 cursor-pointer text-sm">
